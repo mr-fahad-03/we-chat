0a5529a1a92325c2c3770236467dcfed383ddf5d
mr-fahad-03 <mdfahadzulfiqar@gmail.com>
2025-03-04 15:29:31 +0500

    first commit

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,32 @@
+export const THEMES = [
+  "light",
+  "dark",
+  "cupcake",
+ 
+ 
+  "corporate",
+  "synthwave",
+ 
+ 
+  "halloween",
+
+  "forest",
+  "aqua",
+ 
+
+  "fantasy",
+ 
+
+  "luxury",
+ 
+  "cmyk",
+  "autumn",
+
+
+ 
+  "night",
+  "winter",
+  "dim",
+
+  "sunset",
+];
